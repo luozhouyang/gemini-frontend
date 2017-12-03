a17c12fddebebf009db069d1e113643f827070ee
luozhouyang <stupidme.me.lzy@gmail.com>
2017-12-03 21:30:53 +0800

    Route tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  tabs = [
+    {path: '/blogs', label: 'Blogs'},
+    {path: '/about', label: 'About'},
+    {path: '/resume', label: 'Resume'},
+    {path: '/ads', label: 'ADs'}
+  ];
 }
