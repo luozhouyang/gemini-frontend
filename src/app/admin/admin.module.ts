6db62588314834002b4123ecfc5bf35d32653208
luozhouyang <stupidme.me.lzy@gmail.com>
2017-12-05 21:07:15 +0800

    Create login component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from '../login/login.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [DashboardComponent]
+  declarations: [DashboardComponent, LoginComponent]
 })
 export class AdminModule { }
