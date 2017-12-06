32212ba140085e474d00a7c7ad961cdda1e48418
luozhouyang <stupidme.me.lzy@gmail.com>
2017-12-06 22:31:43 +0800

    Fix missing imports

--- src/app/login-module/login/auth.service.ts
+++ src/app/login-module/login/auth.service.ts
@@ -1,5 +1,8 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/delay';
 
 @Injectable()
 export class AuthService {
