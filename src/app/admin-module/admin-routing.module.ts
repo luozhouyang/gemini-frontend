01e5980522bb99e76a6b5eb749acceebaab8992c
luozhouyang <stupidme.me.lzy@gmail.com>
2017-12-10 20:29:27 +0800

    Create article detail component

--- src/app/admin-module/admin-routing.module.ts
+++ src/app/admin-module/admin-routing.module.ts
@@ -1,28 +1,28 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterModule, Routes} from '@angular/router';
-import {AdminComponent} from './admin.component';
-import {AuthGuardService} from '../login-module/login/auth-guard.service';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {EditComponent} from './edit/edit.component';
-import {ArticlesComponent} from './articles/articles.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin.component';
+import { AuthGuardService } from '../login-module/login/auth-guard.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EditComponent } from './edit/edit.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { ArticleDetailComponent } from './article-detail/article-detail.component';
 
 const adminRoutes: Routes = [
   {
     path: '',
     component: AdminComponent,
-    canActive: [AuthGuardService],
+    canActivate: [AuthGuardService],
     children: [
-      {path: 'articles', component: ArticlesComponent},
-      {path: 'edit', component: EditComponent},
-      {path: '', component: DashboardComponent}
+      { path: 'articles', component: ArticlesComponent },
+      { path: 'edit', component: EditComponent },
+      { path: 'article/:id', component: ArticleDetailComponent },
+      { path: '', component: DashboardComponent }
     ]
   }
 ];
 
 @NgModule({
   imports: [
-    CommonModule,
     RouterModule.forChild(adminRoutes)
   ],
   exports: [RouterModule],
