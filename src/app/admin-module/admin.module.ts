e440dbee6cfbef75bddc54d47e5bf4f8f5b96e93
luozhouyang <stupidme.me.lzy@gmail.com>
2017-12-13 23:23:07 +0800

    Create admin header component

--- src/app/admin-module/admin.module.ts
+++ src/app/admin-module/admin.module.ts
@@ -6,13 +6,14 @@ import { EditComponent } from './edit/edit.component';
 import { ArticlesComponent } from './articles/articles.component';
 import { ArticleDetailComponent } from './article-detail/article-detail.component';
 import { AdminRoutingModule } from './admin-routing.module';
+import { AdminHeaderComponent } from './admin-header/admin-header.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AdminRoutingModule
   ],
-  declarations: [DashboardComponent, AdminComponent, EditComponent, ArticlesComponent, ArticleDetailComponent]
+  declarations: [DashboardComponent, AdminComponent, EditComponent, ArticlesComponent, ArticleDetailComponent, AdminHeaderComponent]
 })
 export class AdminModule {
 }
